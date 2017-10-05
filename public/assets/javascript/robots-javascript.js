8a1192b8169946fe8a50d341b600aab1671fd832
Mollert <blueskies@carolina.rr.com>
2017-10-05 18:49:01 -0400

    Fixed the jQuery issue

--- public/assets/javascript/robots-javascript.js
+++ public/assets/javascript/robots-javascript.js
@@ -1,13 +1,7 @@
-console.log("Works");
 
 var units = "metric";
 
-$("#testing").on("click", function() {
-    alert("This is in the javascript");
-});
-
 $(":radio").on("change", function() {
-    console.log("Are we here yet");
 
 	$(":radio").not(':checked').prop("checked", false);
 	if (this.value == "metric") {
@@ -20,12 +14,7 @@ $(":radio").on("change", function() {
         $("#sizeX, #sizeY, #sizeZ, #gravityX, #gravityY, #gravityZ, #robotReach").attr("placeholder", "in");
         $("#partMass").attr("placeholder", "lbs");
     }
-	moving();
 });    
-
-function moving() {
-    console.log(units + " inside");
-};
 
 
 
